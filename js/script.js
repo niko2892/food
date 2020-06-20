21ce0b90b6251545c5195f11dbe9676fc414a97b
Nikolay <niko2892@gmail.com>
2020-06-20 20:05:25 +0300

    tabs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 //tabs
+const tabsParent = document.querySelector('.tabheader__items'),
+      tabsContent = document.querySelectorAll('.tabcontent'),
+      tabsButtons = document.querySelectorAll('.tabheader__item');
+
+function hideTabContent() {
+    tabsContent.forEach(item => {
+        item.style.display = 'none';
+    });
+
+    tabsButtons.forEach(btn => {
+        btn.classList.remove('tabheader__item_active');
+    });
+}
+
+function showTabContent(i = 0){
+    tabsContent[i].style.display = "block";
+
+    tabsButtons[i].classList.add('tabheader__item_active');
+}
+
+hideTabContent();
+showTabContent();
+
+tabsParent.addEventListener('click', (event) => {
+    const target = event.target;
+    
+    if(target && target.classList.contains('tabheader__item')){
+        tabsButtons.forEach((item, i) => {
+            if(target == item){
+                hideTabContent();
+                showTabContent(i);
+            }
+        });
+        
+    }
+});
