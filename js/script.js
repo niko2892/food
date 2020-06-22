64d3acc845574717a3b06be09be4cb7a7dd83474
Nikolay <niko2892@gmail.com>
2020-06-22 14:47:44 +0300

    work with rest operator in class Menu

--- js/script.js
+++ js/script.js
@@ -142,20 +142,27 @@ window.addEventListener('scroll', showModalByScroll);
 
 //work with classes
 class Menu {
-    constructor(src, alt, title, descr, price, parentSelector){
+    constructor(src, alt, title, descr, price, parentSelector, ...classes){ //classes as rest
         this.src = src;
         this.alt = alt;
         this.title = title;
         this.descr = descr;
         this.price = price;
+        this.classes = classes; //это rest operator, поэтому classes - массив!
         this.parent = document.querySelector(parentSelector);
         }
         
         createMenuItem(){
             const div = document.createElement('div');
+            
+            if(this.classes.length === 0) { //провеляю rest оператор. если ничего не было передано
+                this.div = 'menu__item';
+                div.classList.add(this.div); //назначаю по умолчани класс menu__item для div
+            }else{ 
+                this.classes.forEach(className => div.classList.add(className)); //присваиваю div класс из rest оператора classes
+            }
 
             div.innerHTML = `
-            <div class="menu__item">
                 <img src = ${this.src} alt = ${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}"</h3>
                 <div class="menu__item-descr">${this.descr}</div>
@@ -164,33 +171,35 @@ class Menu {
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
-            </div>
             `; 
             this.parent.append(div); 
         }   
 }
 
 new Menu("img/tabs/vegy.jpg",
-"vegy",
-'Меню "Фитнес"',
-'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-'229',
-".menu .container"
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        '229',
+        ".menu .container", //добавляю класс как rest оператор для div
+        'menu__item'
  ).createMenuItem();
 
  new Menu("img/tabs/vegy.jpg",
-"vegy",
-'Меню "Фитнес"',
-'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-'229',
-".menu .container"
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        '229',
+        ".menu .container", //добавляю класс как rest оператор для div
+        'menu__item'
  ).createMenuItem();
 
  new Menu("img/tabs/vegy.jpg",
-"vegy",
-'Меню "Фитнес"',
-'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-'229',
-".menu .container"
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        '229',
+        ".menu .container", //добавляю класс как rest оператор для div
+        'menu__item'
  ).createMenuItem();
  
