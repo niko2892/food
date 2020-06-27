37f2d47faf8ecbd30d8d45decd886080132fb04e
Nikolay <anastasiaakimova@Anastasias-MacBook-Pro.local>
2020-06-27 15:50:47 +0300

    modify calc

--- js/script.js
+++ js/script.js
@@ -428,9 +428,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //calc
     const result = document.querySelector('.calculating__result span');
-    let sex = 'female', 
-    height, weight, age, 
-    ratio = '1.375';
+    let sex,height, weight, age, ratio;
+
+    if (localStorage.getItem('sex')) { //проверка хранилища на наличие данных
+        sex = localStorage.getItem('sex');
+    } else {
+        sex = 'female'; //запись в хранилище
+        localStorage.setItem('sex', 'female');
+    }
+
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio');
+    } else {
+        ratio = 1.375;
+        localStorage.setItem('ratio', 1.375);
+    }
+
+    function initLocalSettings(selector, activeClass) { //установка классов активности кнопок в соответсвии с хранилищем
+        const elements = document.querySelectorAll(selector);
+        elements.forEach(elem => {
+            elem.classList.remove(activeClass);
+            if (elem.getAttribute('id') === localStorage.getItem('sex')){
+                elem.classList.add(activeClass);
+            }
+
+            if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')){
+                elem.classList.add(activeClass);
+            }
+        });
+    }
+
+    initLocalSettings('#gender div', 'calculating__choose-item_active');
+    initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
 
     function calcTotal() {
         if(!sex ||!height ||!weight ||!age ||!ratio){
@@ -447,15 +476,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     calcTotal();
 
-    function getStaticInformation(parentSelector, activeClass) {
-        const elements = document.querySelectorAll(`${parentSelector} div`);
+    function getStaticInformation(selector, activeClass) { //инфо с кнопок
+        const elements = document.querySelectorAll(selector);
 
         elements.forEach(elem => {
             elem.addEventListener('click', (e) => {
                 if(e.target.getAttribute('data-ratio')) {
                     ratio = +e.target.getAttribute('data-ratio');
+                    localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
                 } else {
                     sex = e.target.getAttribute('id');
+                    localStorage.setItem('sex', e.target.getAttribute('id')); //записываю выбор пользователя
                 }
     
                 console.log(ratio, sex);
@@ -468,13 +499,33 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     }
-    getStaticInformation('#gender', 'calculating__choose-item_active');
-    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+    getStaticInformation('#gender div', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
 
-    function getDynamicInfirmation(selector){
-        const input = document.querySelector(selector);
+    function getDynamicInfirmation(selector){ //инфо с инпутов
+        const input = document.querySelector(selector),
+              messageIfNotNumber = document.createElement('div');
+
+        document.querySelector('#constitution')
+        .insertAdjacentElement('afterend', messageIfNotNumber);
+        messageIfNotNumber.style.cssText = `
+            display: none;
+            color: red;
+            text-align: center;
+        `;
+    
+        messageIfNotNumber.textContent = "Ввoдите числa";
 
         input.addEventListener('input', () =>{
+
+            if (input.value.match(/\D/g)) { //если пользователь ввел не число
+                input.style.border = '1.5px solid red';
+                messageIfNotNumber.style.display = '';
+            } else {
+                input.style.border = 'none';
+                messageIfNotNumber.style.display = 'none';
+            }
+
             switch(input.getAttribute('id')){
                 case 'height':
                     height = +input.value;
